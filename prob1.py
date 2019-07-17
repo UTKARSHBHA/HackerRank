864fd7dd12c0e1abad30abbcf876d43bf1a0f51e
UTKARSHBHA <ub2092000@gmial.com>
2019-07-17 22:02:52 +0530

    easy way

--- prob1.py
+++ prob1.py
@@ -1,25 +1,9 @@
 n = int(input("number of students:"))
 
-
-
-b = input("score:")
-A = b.split(" ")
-print(A)
-A = [int(o) for o in A]
-A.sort(reverse = True)
-
-
-print(A)
-
-if A[0] != A[1]:
-    print(A[1])
-else:
-    for x in range(1, n+1):
-        if A[0] != A[x]:
-            print(A[x])
-            break
-
-
+b = map(int, input().split())
+v = list(set(b))
+v.remove(max(v))
+print(max(v))
 
 
 
